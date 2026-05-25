5607da0430fd844513fdf0ad2b407809707f4d72
Nikita B. Zuev <nikitazu@gmail.com>
2026-05-25 19:21:14 +0300

    v3.7.0

--- nzc.h
+++ nzc.h
@@ -2,7 +2,7 @@
 #define NZC_NZC_H
 
 /**
- * Nikita Zuev Common Code Library v3.6.0
+ * Nikita Zuev Common Code Library v3.7.0
  * ======================================
  *
  * [SEC00] ОГЛ Навигация
